afb48119418a15236bccb958caa70255e6dfa891
Getachew Mulat <gech4me@gmail.com>
2021-08-26 15:10:55 +0300

    Website service added

--- app/Services/Subscription/WebsiteService.php
+++ app/Services/Subscription/WebsiteService.php
@@ -2,7 +2,22 @@
 
 namespace App\Services\Subscription;
 
+use App\Http\Resources\DefaultCollectionResource;
+use App\Repositories\Eloquent\WebsiteRepository;
+
 class WebsiteService
 {
+    private $repository;
 
+    public function __construct(WebsiteRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function getWebsites(): DefaultCollectionResource
+    {
+        $websites = $this->repository->all();
+
+        return new DefaultCollectionResource($websites);
+    }
 }
